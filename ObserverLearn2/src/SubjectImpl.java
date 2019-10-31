60917a5f794a73858f7372a2f95df1ab368eed2c
wangzheng <wangzheng@hunktimes.com>
2019-10-31 19:41:12 +0800

    【观察者模式】第二种写法优化

--- ObserverLearn2/src/SubjectImpl.java
+++ ObserverLearn2/src/SubjectImpl.java
@@ -13,10 +13,13 @@ public abstract class SubjectImpl implements Subject {
     @Override
     public void del(ObjectObserver observer) {
         observers.remove(observer);
+        System.out.println("remove observer:" + observer.getClass().getName());
     }
 
     @Override
     public void notifyObserver(int state) {
-        observers.forEach(observer -> observer.update(state));
+        observers.forEach(observer -> {
+            observer.update(state);
+            System.out.println("通知观察者：" + observer.getClass().getName() + "，执行update方法");});
     }
 }
